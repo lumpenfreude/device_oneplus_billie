20f0efb6b44420d14f0b5546cff14f61155261dd
elen <elen.e.parker@gmail.com>
2021-05-18 11:10:29 +0200

    whatevs

--- device.mk
+++ device.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
- Camera
+# cam3rwa
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
 	android.hardware.camera.provider.2.4-impl \
